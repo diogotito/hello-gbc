cc43615ab6d499a8d8dc7325b5d8411c6ba1e032
diogotito <diogotitomarques@gmail.com>
2025-07-09 23:30:15 +0100

    BACKGROUND GET!

--- src/main.c
+++ src/main.c
@@ -1,14 +1,27 @@
 #include <gb/gb.h>
+#include <gb/cgb.h>
+#include <gbdk/incbin.h>
 #include <stdint.h>
-#include "../res/dungeon_map.h"
-#include "../res/dungeon_tiles.h"
+#include "../res/ruins.h"
 
+// city tiles
+
+
+#define TILE_BYTES 16       // 16 bytes per background tile
 
 void init_gfx(void) {
-    // Load Background tiles and then map
-    set_bkg_data(0, 79u, dungeon_tiles);
-    set_bkg_tiles(0, 0, 32u, 32u, dungeon_mapPLN0);
+    // Load background tile patterns
+    set_bkg_data(0, ruins_TILE_COUNT, ruins_tiles);
 
+    // Transfer color palettes
+    set_bkg_palette(0, ruins_PALETTE_COUNT, ruins_palettes);
+    
+    // Load background attributes and map
+    VBK_REG = VBK_ATTRIBUTES;
+    set_bkg_tiles(0, 0, ruins_MAP_ATTRIBUTES_WIDTH, ruins_MAP_ATTRIBUTES_HEIGHT, ruins_map_attributes);
+    VBK_REG = VBK_TILES;
+    set_bkg_tiles(0, 0, ruins_MAP_ATTRIBUTES_WIDTH, ruins_MAP_ATTRIBUTES_HEIGHT, ruins_map);
+    
 	// Turn the background map on to make it visible
     SHOW_BKG;
 }
