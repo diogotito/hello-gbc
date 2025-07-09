0c08b443b5ff09fe52e9f586302796cf1eef6849
diogotito <diogotitomarques@gmail.com>
2025-07-09 15:50:29 +0100

    first commit

---
+++ compile.bat
@@ -0,0 +1,6 @@
+REM Automatically generated from Makefile
+mkdir obj
+C:\tools\gbdk\bin\lcc   -c -o obj\main.o src\main.c
+C:\tools\gbdk\bin\lcc   -c -o obj\dungeon_map.o res\dungeon_map.c
+C:\tools\gbdk\bin\lcc   -c -o obj\dungeon_tiles.o res\dungeon_tiles.c
+C:\tools\gbdk\bin\lcc   -o obj\Example.gb obj\main.o obj\dungeon_map.o obj\dungeon_tiles.o 
